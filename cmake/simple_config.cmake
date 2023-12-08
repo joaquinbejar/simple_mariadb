9d6ef8544612d175e0961117c20177911c6ca5ed
Joaquin Bejar <jb@taunais.com>
2023-12-08 07:33:12 +0100

    add some unit tests for config.h and client.h

--- cmake/simple_config.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(simple_config
-        GIT_REPOSITORY https://github.com/joaquinbejar/simple_config.git
-        GIT_TAG dev
-        )
-FetchContent_MakeAvailable(simple_config)
-
-set(SIMPLE_CONFIG_INCLUDE ${simple_config_SOURCE_DIR}/include CACHE INTERNAL "")
